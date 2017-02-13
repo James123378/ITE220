2147bc3972d2b6fe03599d3d9053583e2adecf82
James123378 <James@chindasia.com>
2017-02-13 18:59:41 +0700

    Create shop.js

---
+++ js/shop.js
@@ -0,0 +1,7 @@
+var customerName= "Mintra";
+Var price =10;
+var quantity = 2;
+var totalPrice = price * quantity;
+
+var customerElement = document.getElementById("customer-name");
+customerElement.textContent = customerName;
